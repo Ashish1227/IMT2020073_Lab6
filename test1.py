73baef957401b02e8dd9a4a6c1ed9d5557aabd4d
Ashish1227 <72496006+Ashish1227@users.noreply.github.com>
2022-11-24 17:47:47 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,14 +5,27 @@ from function import muliplication
 
 class TestMult(unittest.TestCase):
   
+    """
+    Test cases to multiply two numbers
+    """
+  
     def test_list_int(self):
-        """
-        Test case to multiply two numbers
-        """
         test_a = 2
         test_b = 4
         result = multiplication(test_a,test_b)
         self.assertEqual(result, 8)
+       
+    def test_list_int2(self):
+        test_a = 3
+        test_b = 4
+        result = multiplication(test_a,test_b)
+        self.assertEqual(result, 12)
+       
+    def test_list_int3(self):
+        test_a = 8
+        test_b = 2
+        result = multiplication(test_a,test_b)
+        self.assertEqual(result, 16)      
 
 if __name__ == '__main__':
     unittest.main()
